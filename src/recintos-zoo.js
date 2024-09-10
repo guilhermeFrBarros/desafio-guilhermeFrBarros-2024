15908e08b3943311585365df61ac4ef1c864b6eb
guilhermeFrBarros <guilhermegoveiabarros@outlook.com>
2024-09-10 09:50:09 -0300

    feat: commit inicial com a estrutura base para o desafio

---
+++ src/recintos-zoo.js
@@ -0,0 +1,16 @@
+import Animal from "./Animal";
+
+class RecintosZoo {
+
+    analisaRecintos(animal, quantidade) {
+
+
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
+
+
+// Vou ter criar uma classe a animal
+// E uma classe recinto
